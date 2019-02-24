5f3465993415cbec9bf58dbeceab7722e6f1108d
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-02-24 22:42:12 +0800

    练习

---
+++ src/main/java/com/imooc/ConditionDemo1.java
@@ -0,0 +1,23 @@
+package com.imooc;
+
+public class ConditionDemo1 {
+    public static void main(String[] args) {
+        //两件商品的价格大于100则减20，并把原价和折后价分别输出
+        double price1,price2;
+        price1=80;
+        price2=15;
+        //计算总价
+        double sum=price1+price2;
+        System.out.println("原价为："+sum);
+
+        if (sum>100){
+            sum=sum-20;
+        System.out.println("折后价："+sum);
+
+        }
+        else
+        System.out.println("不满100不打折");
+
+    }
+
+}
--- src/main/java/com/imooc/HelloImooc.java
+++ src/main/java/com/imooc/HelloImooc.java
@@ -1,5 +1,7 @@
+package com.imooc;
+
 public class HelloImooc {
     public static void main(String[]args){
-        System.out.println("Hello Imooc");
+        System.out.println("Hello,Imooc!");
     }
 }
