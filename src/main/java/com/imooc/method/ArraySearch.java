c10f4c0a48094bc1c6c0f81cff2a72f39ecc50fe
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-03-10 18:53:10 +0800

    方法练习

---
+++ src/main/java/com/imooc/method/ArraySearch.java
@@ -0,0 +1,22 @@
+package com.imooc.method;
+
+public class ArraySearch {
+    public boolean arraySearch(int n,int[]intArry){
+        boolean flag=false;
+        for (int i=0;i<intArry.length;i++){
+            if(n==intArry[i])
+                flag=true;
+        }
+        return flag;
+    }
+    public static void main(String []args){
+        int n=301;
+        int [] intArry={10,20,30,40};
+        ArraySearch a=new ArraySearch();
+        if (a.arraySearch(n,intArry)==true)
+        System.out.println("找到了");
+        else
+            System.out.println("没找到这个数");
+
+    }
+}
