9e5f9f12b211cc0afc72211210051829635b01a0
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-03-20 14:06:36 +0800

    静态代码块执行顺序

--- src/main/java/com/imooc/statictest/CodeBlock.java
+++ src/main/java/com/imooc/statictest/CodeBlock.java
@@ -1,10 +1,18 @@
 package com.imooc.statictest;
+/*  执行结果：静态代码块执行顺序>构造块>构造方法，静态代码块只执行一次
+CodeBlock的静态代码块
+CodeBlock的主方法
+产生Code类实例对象
+Code的静态代码块
+Code的构造块
+Code的构造方法
+产生CodeBlock类实例对象
+CodeBlock的构造块
+CodeBlock的构造方法
+* */
 
 public class CodeBlock{
-    // 创建CodeBlock的构造块
-    {
-        System.out.println("CodeBlock的构造块");
-    }
+
     //创建CodeBlock的静态代码块
     static {
         System.out.println("CodeBlock的静态代码块");
@@ -15,6 +23,10 @@ public class CodeBlock{
     CodeBlock()
     {
         System.out.println("CodeBlock的构造方法");
+    }
+    // 创建CodeBlock的构造块
+    {
+        System.out.println("CodeBlock的构造块");
     }
 
 
