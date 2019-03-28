53f1723601177dba74a1fa12be70263c3b2113f3
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-03-28 22:52:11 +0800

    新建类

---
+++ src/main/java/com/imooc/extend/Test.java
@@ -0,0 +1,16 @@
+package com.imooc.extend;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("父类信息测试：");
+
+        System.out.print("自行车类信息测试：");
+        Bicycle bicycle=new Bicycle();
+        System.out.print(bicycle.work());
+
+        System.out.print("电动车类信息测试：");
+
+        System.out.print("三轮车类信息测试：");
+
+    }
+}
