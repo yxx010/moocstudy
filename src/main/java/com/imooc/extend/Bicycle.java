5e2ceef5a8bcdbbf03421a7422313b4636ca66c7
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-03-30 13:07:06 +0800

    增加电动车类

--- src/main/java/com/imooc/extend/Bicycle.java
+++ src/main/java/com/imooc/extend/Bicycle.java
@@ -2,9 +2,11 @@ package com.imooc.extend;
 
 public class Bicycle extends NonMotor {
     // 在构造方法中调用父类多参构造，完成属性赋值
-    public Bicycle(){};
-    public Bicycle(String brand,String color){
+    public Bicycle(){
+    }
 
+    public Bicycle(String brand,String color){
+        super(brand,color);
     }
 
     // 重写运行方法，描述内容为：这是一辆**颜色的，**牌的自行车。其中**的数据由属性提供
