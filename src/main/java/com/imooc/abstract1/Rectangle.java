5e795b16cdc4161511f7eb2bfe3be953f3513884
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-04-18 14:50:11 +0800

    抽象类

---
+++ src/main/java/com/imooc/abstract1/Rectangle.java
@@ -0,0 +1,38 @@
+package com.imooc.abstract1;
+
+public class Rectangle extends Shape {
+    //属性：矩形的长lenghth、宽wide
+    int length;
+    int wide;
+    //创建带参构造方法以及无参构造方法
+    public Rectangle(){
+    }
+    public Rectangle(int length,int wide){
+        this.setLength(length);
+        this.setWide(wide);
+    }
+    //创建针对长、宽的赋值和取值方法
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWide() {
+        return wide;
+    }
+
+    public void setWide(int wide) {
+        this.wide = wide;
+    }
+    //重写父类的area（）方法
+    public double area(){
+        double s;
+        s=length*wide;
+        return s;
+    }
+}
+
---
+++ src/main/java/com/imooc/abstract1/Shape.java
@@ -0,0 +1,9 @@
+package com.imooc.abstract1;
+
+//抽象类Shape，里面含抽象方法area( )
+abstract class Shape{
+
+    public abstract double area();
+
+}
+
