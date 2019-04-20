69d3793b727e125daf07d3ef810937f1d0052dea
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-04-20 15:22:45 +0800

    接口及接口实现

---
+++ src/main/java/com/imooc/implementsTest/Bird.java
@@ -0,0 +1,7 @@
+package com.imooc.implementsTest;
+
+public class Bird implements IFly {
+    public void fly() {
+        System.out.println("小鸟在天上飞");
+    }
+}
---
+++ src/main/java/com/imooc/implementsTest/Test.java
@@ -0,0 +1,12 @@
+package com.imooc.implementsTest;
+
+public class Test {
+    public static void main(String[] args) {
+        Plane plane=new Plane();
+        Bird bird=new Bird();
+        plane.fly();
+        bird.fly();
+    }
+
+
+}
