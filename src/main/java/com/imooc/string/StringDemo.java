a9db1ebe2fb6f54efeef9d3e1530cd6b110239c7
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-04-28 12:48:54 +0800

    String练习

---
+++ src/main/java/com/imooc/string/StringDemo.java
@@ -0,0 +1,12 @@
+package com.imooc.string;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        //定义字符串"abc"
+        StringBuilder str=new StringBuilder("abc");
+        //经过处理得到字符串"fed---cba"
+        System.out.println(str.replace(0,3,"fed---cba"));
+        //进一步变换得到字符串"fedcba"
+        System.out.println(str.delete(3,6));
+    }
+}
---
+++ src/main/java/com/imooc/string/StringDemo1.java
@@ -0,0 +1,18 @@
+package com.imooc.string;
+
+import java.util.Locale;
+
+public class StringDemo1 {
+    public static void main(String[] args) {
+		/*已知字符串"abcdefg"，使之经过处理得到字符串"CDE",并进一步
+        将“DE”替换为“MM”，得到结果：CMM */
+		String str="abcdefg";
+        System.out.println(str);
+        str=str.substring(2,5);
+		str=str.toUpperCase();
+        System.out.println(str);
+		str=str.replace("DE","MM");
+        System.out.println(str);
+    }
+
+}
