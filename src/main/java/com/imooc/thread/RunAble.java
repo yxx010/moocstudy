63df08a4ccaf7fe53d2f99a280c033f908d5fafd
yangxiaoxiao <yxx_beijing@163.com>
2019-05-07 23:30:50 +0800

    线程

--- src/main/java/com/imooc/thread/RunAble.java
+++ src/main/java/com/imooc/thread/RunAble.java
@@ -2,9 +2,17 @@ package com.imooc.thread;
 
 public class RunAble implements Runnable {
     public void run() {
+        System.out.println(Thread.currentThread().getName()+"线程正在运行");
     }
 
     public static void main(String[] args) {
+        RunAble  ra=new RunAble();
+        Thread t1=new Thread(ra);
+        t1.start();
+
+        RunAble  ra1=new RunAble();
+        Thread t2=new Thread(ra1);
+        t2.start();
 
     }
 }
