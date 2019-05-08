9feafae533b149789da00741bef037e59eed752d
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-05-08 10:05:09 +0800

    runnable创建练习

--- src/main/java/com/imooc/thread/Runnable.java
+++ src/main/java/com/imooc/thread/Runnable.java
@@ -1,16 +1,16 @@
 package com.imooc.thread;
 
-public class RunAble implements Runnable {
+public class Runnable implements java.lang.Runnable {
     public void run() {
         System.out.println(Thread.currentThread().getName()+"线程正在运行");
     }
 
     public static void main(String[] args) {
-        RunAble  ra=new RunAble();
+        Runnable ra=new Runnable();
         Thread t1=new Thread(ra);
         t1.start();
 
-        RunAble  ra1=new RunAble();
+        Runnable ra1=new Runnable();
         Thread t2=new Thread(ra1);
         t2.start();
 
