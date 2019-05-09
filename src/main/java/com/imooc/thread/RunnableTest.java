1914242496310e84561afa720cd76e2b9d828cb2
yangxiaoxiao <yxx_beijing@163.com>
2019-05-09 23:14:24 +0800

    线程

--- src/main/java/com/imooc/thread/RunnableTest.java
+++ src/main/java/com/imooc/thread/RunnableTest.java
@@ -1,16 +1,16 @@
 package com.imooc.thread;
 
-public class Runnable implements java.lang.Runnable {
+public class RunnableTest implements java.lang.Runnable {
     public void run() {
         System.out.println(Thread.currentThread().getName()+"线程正在运行");
     }
 
     public static void main(String[] args) {
-        Runnable ra=new Runnable();
+        RunnableTest ra=new RunnableTest();
         Thread t1=new Thread(ra);
         t1.start();
 
-        Runnable ra1=new Runnable();
+        RunnableTest ra1=new RunnableTest();
         Thread t2=new Thread(ra1);
         t2.start();
 
