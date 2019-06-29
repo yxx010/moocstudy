e7487731ea7a51c9ab33b6c20f707c05737d1cb3
yangxiaoxiao <yxx_beijing@163.com>
2019-06-29 18:18:38 +0800

    计算器作业完成

--- src/main/webapp/step2/2.仿计算器/cal.js
+++ src/main/webapp/step2/2.仿计算器/cal.js
@@ -3,6 +3,8 @@ function init() {
     num.value=0;
     num.disabled="disabled";
     var oButton=document.getElementsByTagName("input");
+    var tempNum;
+    var fh;
     for (var i=0;i<oButton.length;i++){
         oButton[i].onclick=function(){
             if(isNumber(this.value)){
@@ -24,34 +26,96 @@ function init() {
                 //alert(btn_num);
                 switch (btn_num) {
                     case "+":
-                        break
+                        //alert("+");
+                        tempNum=num.value*1;
+                        num.value=0;
+                        fh="+";
+                        break;
                     case "-":
+                        tempNum=num.value*1;
+                        num.value=0;
+                        fh="-";
                         break;
                     case "*":
+                        tempNum=num.value*1;
+                        num.value=0;
+                        fh="*";
                         break;
                     case "/":
+                        tempNum=num.value*1;
+                        num.value=0;
+                        fh="/";
                         break;
                     case ".":
+                        num.value=dec_number(num.value);
+                        break;
+                    case "←":
+                        num.value=back(num.value);
                         break;
                     case "c":
+                        num.value=0;
+                        break;
+                    case "+/-":
+                        num.value=sign(num.value);
+                        break;
+                    case "m":
+                        break;
+                    case "=":
+                        switch (fh) {
+                            case "+":
+                                num.value=tempNum+num.value*1;
+                                break;
+                            case "-":
+                                num.value=tempNum-num.value*1;
+                                break;
+                            case "*":
+                                num.value=tempNum*num.value*1;
+                                break;
+                            case "/":
+                                if(num.value==0)
+                                alert("除数不能为0！")
+                                num.value=tempNum/num.value;
+                                break;
+                        }
                         break;
                 }
             }
         }
     }
 }
+/*判断是否是数字*/
 function isNumber(n) {
     return !isNaN(n);
 }
+/*isnull代表是字符，不能转成数字*/
 function isNull(n) {
-    //if((n=="0"||n.length==0))
-    //{return true;}else{
-    //    return false;
-    //}
-
     return ((n=="0"||n.length==0));
-    /*{return true;}else{
-        return false;
-    }*/
-
+}
+/*小数点*/
+function dec_number(n) {
+    if(n.indexOf(".")==-1){
+        n=n+".";
+    }
+    return n;
+}
+/*退位键←*/
+function back(n) {
+    n=n.substr(0,n.length-1);
+    if(isNull(n)){
+       n=0;
+    }
+    return n;
+}
+/*正负号*/
+function sign(n) {
+    if(n==0)
+    {
+        return n;
+    }
+    else if(n.indexOf("-")==-1){
+        n="-"+n;
+    }else{
+        n=n.substr(1,n.length);
+    }
+    return n;
 }
---
+++ src/main/webapp/step2/2.仿计算器/js/mooc.js
@@ -0,0 +1,5 @@
+function init_mooc(){
+    document.getElementById("m_id").onclick=function(){
+        window.location.href="http://www.imooc.com"
+    }
+}
