130b84083ba37e07a0d20eb39a6d626a21db937f
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-07-09 15:56:14 +0800

    header制作

---
+++ src/main/webapp/step2/3.前端优化商城/js/1.js
@@ -0,0 +1,20 @@
+var flag=true;
+function show_menu(){
+    var menu1=document.getElementById("menu1");
+    /*显示目录*/
+    if(flag){
+        menu1.style.display="block";
+        flag=false;
+    }else{
+        menu1.style.display="none";
+        flag=true;
+    }
+
+}
+function show_menu1() {
+    var menu1=document.getElementById("menu1");
+    if(!flag){
+        menu1.style.display="none";
+        flag=true;
+    }
+}
