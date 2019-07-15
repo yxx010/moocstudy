ef4bbb13732c66b60d2e524dd6627450dbcbdd5e
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-07-15 13:31:34 +0800

    轮播小圆点增加事件

--- src/main/webapp/step2/4.JavaScript轮播图/js/轮播.js
+++ src/main/webapp/step2/4.JavaScript轮播图/js/轮播.js
@@ -3,13 +3,17 @@ function byId(id){
    return typeof(id)==="string"?document.getElementById(id):id;
 }
 console.log(byId("main"));
-var index=0,timer=null,pics=byId("banner").getElementsByTagName("div"),
-len=pics.length;
+var index=0,timer=null,
+    pics=byId("banner").getElementsByTagName("div"),
+    dots=byId("dots").getElementsByTagName("span"),
+    len=pics.length;
 console.log(len);
 function slideImg() {
-    var  main=byId("main");
+    var main=byId("main");
     main.onmouseover=function(){
-
+        //鼠标悬停时，不轮播
+        if(timer)
+            clearInterval(timer);
     }
     main.onmouseout=function(){
         timer=setInterval(function(){
@@ -20,6 +24,16 @@ function slideImg() {
             //console.log(index);
             changeImg();
         },3000);
+    }
+    //自动再main上触发鼠标离开事件
+    main.onmouseout();
+    //遍历所有点击，且绑定点击事件，点击圆点切换图片
+    for (var d=0;d<len;d++){
+        dots[d].id=d;
+        dots[d].onclick=function(){
+            //改变index为当前span的id值
+            alert(this.id);
+        }
     }
 }
 function changeImg(){
