95710231471963ade81c3f115a1b7274271c9f9b
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-07-26 14:02:37 +0800

    xpathtest

---
+++ src/main/java/com/imooc/stage2/step3/part1/xml/XPathTest.java
@@ -0,0 +1,41 @@
+package com.imooc.stage2.step3.part1.xml;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import java.util.List;
+
+public class XPathTest {
+    public void xpath(String xpathExp){
+        String file="plan.xml";
+        SAXReader reader=new SAXReader();
+        try {
+            Document document=reader.read(file);
+            List<Node> nodes=document.selectNodes(xpathExp);
+            for (Node node:nodes){
+                Element element=(Element)node;
+                System.out.println(element.attributeValue("no"));
+                System.out.println(element.elementText("course-name"));
+                System.out.println(element.elementText("class-hour"));
+                System.out.println(element.elementText("exam-form"));
+                System.out.println("===================");
+
+            }
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        XPathTest test=new XPathTest();
+        //test.xpath("/plan/item");
+        //test.xpath("//item");
+        test.xpath("//item[class-hour>100]");
+        test.xpath("//item[course-name='大学英语']");
+
+
+    }
+}
