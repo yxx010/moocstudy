d3424f89aa7c1f999da5dbb94e4339a33190c5db
yangxiaoxiao <yxx_beijing@163.com>
2019-09-07 14:58:45 +0800

    xml

---
+++ src/main/java/com/imooc/stage1/file/MultipartFileDemo.java
@@ -0,0 +1,12 @@
+package com.imooc.stage1.file;
+
+import java.io.File;
+
+public class MultipartFileDemo {
+    public static void main(String[] args) {
+        FileDemo fileDemo=new FileDemo();
+        //MultipartFile multipartFile=fileDemo.getFile();
+
+    }
+
+}
