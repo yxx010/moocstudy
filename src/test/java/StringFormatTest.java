252e1718ac73bbe1b9a9c2c511b68e833b48e2c0
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-11-19 11:00:50 +0800

    StringFormatTest

---
+++ src/test/java/StringFormatTest.java
@@ -0,0 +1,19 @@
+public class StringFormatTest {
+    public static void main(String[] args) {
+
+        int userNum = 100;
+        String oldNickName="ytest";
+        //默认格式化规则
+        String str="%01d";
+
+        int len=String.valueOf(userNum).length();
+        //昵称格式化规则
+        str = "%0" + (len) + "d";
+        System.out.println(str);
+        for (int i = 0; i < userNum; i++) {
+            String nickname = oldNickName + String.format(str,i + 1);
+            System.out.println(nickname);
+        }
+    }
+
+}
