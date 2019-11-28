2b606563cf46a41a5b3807f9a1fe9208b0d7f2a8
yangxiaoxiao <yxx_beijing@163.com>
2019-11-28 22:23:33 +0800

    jdbc练习

--- src/main/java/com/imooc/stage3/step1/JdbcDemo2.java
+++ src/main/java/com/imooc/stage3/step1/JdbcDemo2.java
@@ -18,8 +18,9 @@ public class JdbcDemo2 {
             String create_sql="CREATE TABLE IF NOT EXISTS goods (id INT PRIMARY KEY auto_increment COMMENT '商品编号',NAME VARCHAR (20) COMMENT '商品名称',price FLOAT COMMENT '商品价格',desp VARCHAR (30) COMMENT '商品描述')";
             String insert_sql="INSERT goods (name,price,desp)VALUEs('手机',2000,'黑色，存储32G'),('冰箱',15000,'银色，对开门'),('洗衣机',3000,'滚桶'),('空调',4000,'变频空调')";
             Statement statement=connection.createStatement();
-            statement.executeUpdate(create_sql);
-            statement.executeUpdate(insert_sql);
+            Integer line1=statement.executeUpdate(create_sql);
+            Integer line2=statement.executeUpdate(insert_sql);
+            System.out.println("line1="+line1+",line2="+line2);
             ResultSet resultSet=statement.executeQuery("select * from goods");
             while (resultSet.next()){
                 String name=resultSet.getString("name");
