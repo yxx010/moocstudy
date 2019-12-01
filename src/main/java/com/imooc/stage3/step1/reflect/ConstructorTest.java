db0e8ad16a3fe55d3d109edc2a8a477d7d4f6cbb
yangxiaoxiao <yxx_beijing@163.com>
2019-12-01 22:38:12 +0800

    java反射练习

---
+++ src/main/java/com/imooc/stage3/step1/reflect/ConstructorTest.java
@@ -0,0 +1,30 @@
+package com.imooc.stage3.step1.reflect;
+
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+
+public class ConstructorTest {
+    @Test
+    /**
+     * 获得无参构造，并且实例化
+     */
+    public void demo1() throws Exception {
+        Class class1=Class.forName("com.imooc.stage3.step1.reflect.Person");
+        Constructor constructor=class1.getConstructor();
+        //相当于Person person=new Person();
+        Person person=(Person) constructor.newInstance();
+        person.eat();
+    }
+    @Test
+    /**
+     * 获得有参数的构造方法
+     */
+    public void demo2() throws Exception {
+        Class class1=Class.forName("com.imooc.stage3.step1.reflect.Person");
+        Constructor constructor=class1.getConstructor(String.class,String.class);
+        //相当于Person person=new Person("张三","男");
+        Person person=(Person)constructor.newInstance("张三","男");
+        System.out.println(person);
+    }
+}
