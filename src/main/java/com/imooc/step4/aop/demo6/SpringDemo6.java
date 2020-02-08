25c3961ac47f95ea724ed77c5d19ff11669573a4
yangxiaoxiao <yxx_beijing@163.com>
2020-02-08 17:25:59 +0800

    aop demo 5、6

---
+++ src/main/java/com/imooc/step4/aop/demo6/SpringDemo6.java
@@ -0,0 +1,35 @@
+package com.imooc.step4.aop.demo6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+//一般切面案例
+//需要在pom里增加spring-test的依赖才能用这个注解，step>ioc下有不使用注解的方式
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext6.xml")
+public class SpringDemo6 {
+    //@Resource(name="customerDao")
+    @Resource(name="customerDao")
+    private CustomerDao customerDao;
+
+    @Resource(name="studentDao")
+    private StudentDao studentDao;
+
+    @Test
+    public void demo1(){
+        customerDao.find();
+        customerDao.find1();
+        customerDao.save();
+        customerDao.update();
+        customerDao.delete();
+
+        studentDao.find();
+        studentDao.save();
+        studentDao.update();
+        studentDao.delete();
+    }
+}
---
+++ src/main/java/com/imooc/step4/aop/demo6/StudentDao.java
@@ -0,0 +1,8 @@
+package com.imooc.step4.aop.demo6;
+
+public interface StudentDao {
+    public void find();
+    public void save();
+    public void update();
+    public void delete();
+}
