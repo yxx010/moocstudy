2edd9750569952f7a6d2adf9da401ef0bd55096d
yxx010 <yxx_beijing@163.com>
2021-03-22 17:06:42 +0800

    补充变量demo

---
+++ src/main/java/com/imooc/basic/flow1/StringDemo.java
@@ -0,0 +1,14 @@
+package com.imooc.basic.flow1;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String s1="";
+        System.out.println("s1="+s1);
+        String s2="hello";
+        System.out.println("s2="+s2);
+        String s3="A\u005d\u005fB";
+        System.out.println("s3="+s3);
+        String s4="hello imooc";
+        System.out.println("s4="+s4);
+    }
+}
---
+++ src/main/java/com/imooc/basic/flow1/VarDemo.java
@@ -0,0 +1,18 @@
+package com.imooc.basic.flow1;
+
+public class VarDemo {
+    public static void main(String[] args) {
+        //定义2个整形变量
+        int x = 0,y = 0;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+
+        System.out.println(x+"\t"+y);
+        double d=1.24E5;
+        System.out.println(d);
+        double d1=.2;
+        System.out.println(d1);
+
+
+    }
+}
